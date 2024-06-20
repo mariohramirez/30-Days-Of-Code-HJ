832959767ad13d8ace8c0d7c0b19c52d08479fea
Mario Ramírez <marioh.ramirez@udea.edu.co>
2024-06-20 15:25:41 -0500

    Primer ejercicio

---
+++ Day 0 - Hello World/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+F
+src/com/code/Main.java,d/3/d3604e9223d85232c3b8399a4448a50bcf1287db
+J
+src/com/code/Solution.java,0/6/06f9005c5e7118fb1fd1f9ae7b3019a98bcd3041
---
+++ Day 0 - Hello World/src/com/code/Solution.java
@@ -0,0 +1,17 @@
+package com.code;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String message = scanner.nextLine();
+
+        System.out.println("Hello, World.");
+        System.out.println(message);
+
+    }
+}
